8a48f46ccb8fd9baabb65f8c394fb6b9f54c372b
NicolasBusta <nicolasbustamanteb1@gmail.com>
2022-05-10 13:42:01 -0300

    se descomenta

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ let edad = prompt("¿que edad tienes?");
     }
 */
 //objetos(cervezas):
-/*
+
 class Cerveza{
    
     constructor( nombre, color, ibu) {
@@ -39,7 +39,7 @@ const CervezaTrigo = new Cerveza("weissen", "rubia", 20);
 
 
 const Cervezas =[CervezaGoldenAle, CervezasPaleAle,CervezaOktoberfest];
-*/
+
 
 // liosta desplegable de opciones:
 /*
@@ -86,24 +86,62 @@ function RandomSugerencia(array) {
         cambiar();
     }
 */
-//eventos:
+//eventos Y MODIFICACIONES EN EL dom:
+//rubia detalles:
 let detalleRubia ="refrescant, ligera equilibrada";
 let detalleRoja="dulce, maltosa y caramelosa";
 let detalleNegra="fuerte, tostada, sabor a cafe o chocolate";
 
-let contenedor = document.getElementById("contenedor");
-let boton = document.getElementById('bot1');
+let contenedor1 = document.getElementById("contenedor1");
+let contenedor2 = document.getElementById("contenedor2");
+let contenedor3 = document.getElementById("contenedor3");
 
+let boton1 = document.getElementById('bot1');
 let boton2 = document.getElementById('bot2');
 let boton3 = document.getElementById('bot3');
 
 
-boton.addEventListener("click",()=>{
+boton1.addEventListener("click",()=>{
 
     let detalle1= detalleRubia;
-    let parrafo = document.createElement("p");
-    parrafo.innerHTML = detalle1;
-    contenedor.append(parrafo);
+    let parrafo1 = document.createElement("p");
+    parrafo1.innerHTML = detalle1;
+    contenedor1.append(parrafo1);
+});
+//roja detalles:
+boton2.addEventListener("click",()=>{
+
+    let detalle2= detalleRoja;
+    let parrafo2 = document.createElement("p");
+    parrafo2.innerHTML = detalle2;
+    contenedor2.append(parrafo2);
+});
+//negra detalles:
+boton3.addEventListener("click",()=>{
+
+    let detalle3= detalleNegra;
+    let parrafo3 = document.createElement("p");
+    parrafo3.innerHTML = detalle3;
+    contenedor3.append(parrafo3);
 });
 
 
+//storage y JASON:
+ let productos =[
+     {
+        id: 1,
+        nombre: "Golden Ale",
+     },
+    {
+        id: 2,
+        nombre: "Cervezas Pale Ale",
+    },
+];
+
+const guardarLocalmente =(clave,valor)=>{
+    localStorage.setItem(clave, valor)
+};
+
+for (const producto of Cervezas){
+    guardarLocalmente(producto.nombre, JSON.stringify(producto));
+}
